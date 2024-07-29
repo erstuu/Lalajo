2f2d0585bb6812f4b00aca8ea0c58b1abc530e15
Restu Gede Purnama <restumelenoy83@gmail.com>
2024-07-29 07:46:44 +0700

    added navigation in SearchIcon navigate to SearchScreen

--- components/home/MovieHeader.js
+++ components/home/MovieHeader.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { View, TouchableWithoutFeedback, Image } from "react-native";
-// import FastImage from "react-native-fast-image";
+import { useNavigation } from "@react-navigation/native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 import MenuIcon from "../../assets/open-menu.png";
@@ -14,19 +14,22 @@ const Header = () => {
     );
 }
 
-const SearchIcon = () => {
+const SearchIcon = ({type}) => {
+    const navigation = useNavigation();
     return(
-        <TouchableWithoutFeedback>
+        <TouchableWithoutFeedback
+            onPress={() => navigation.navigate("Search", { type: type })}
+        >
             <Ionicons name="search-outline" size={25} color={black} />
         </TouchableWithoutFeedback>
     );
 }
 
-const MovieHeader = () => {
+const MovieHeader = ({type}) => {
     return(
         <View style={{ flexDirection: "row", justifyContent: "space-between", padding: 16 }}>
             <Header />
-            <SearchIcon />
+            <SearchIcon type={type}/>
         </View>
     );
 }
